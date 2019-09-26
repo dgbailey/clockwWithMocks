22401f415d70a6bbec4548d1cbd7c6c43fe72bd3
dgbailey <bailey.dust@gmail.com>
2019-09-26 10:57:12 -0700

    added mocks to rect clock. Need to figure out dependency setup for non create react app environment

---
+++ src/Fetch.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+export default function Fetch({ url }) {
+  const [greeting, setGreeting] = useState('')
+  const [buttonClicked, setButtonClicked] = useState(false)
+
+  const fetchGreeting = async () => {
+    const response = await axios.get(url)
+    const data = response.data
+    const { greeting } = data
+    setGreeting(greeting)
+    setButtonClicked(true)
+  }
+
+  const buttonText = buttonClicked ? 'Ok' : 'Load Greeting'
+
+  return (
+    <div>
+      <button
+        onClick={fetchGreeting}
+        data-testid="ok-button"
+        disabled={buttonClicked}
+      >
+        {buttonText}
+      </button>
+      {greeting ? <h1 data-testid="greeting-text">{greeting}</h1> : null}
+    </div>
+  )
+}
+
+//pretty sure not having the create react app enviroment is the issue I am encountering.  This works in react clock.
---
+++ src/Fetch.test.js
@@ -0,0 +1,28 @@
+// __tests__/fetch.test.js
+import React from 'react';
+import { render, fireEvent, waitForElement } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
+import axiosMock from 'axios';
+import Fetch from './Fetch';
+
+test('loads and displays greeting', async () => {
+  const url = '/greeting'
+  const { getByText, getByTestId, ...rest} = render(<Fetch url={url} />)
+  
+  axiosMock.get.mockResolvedValueOnce({
+    data: { greeting: 'hello there' },
+  })
+
+
+  fireEvent.click(getByText('Load Greeting')) 
+
+  const greetingTextNode = await waitForElement(() =>
+    getByTestId('greeting-text')
+  ) 
+
+  expect(axiosMock.get).toHaveBeenCalledTimes(1)
+  expect(axiosMock.get).toHaveBeenCalledWith(url)
+  expect(getByTestId('greeting-text')).toHaveTextContent('hello there')
+  expect(getByTestId('ok-button')).toHaveAttribute('disabled')
+  console.log(rest.debug()) 
+})
